83d611fc7c20933f1fd4bdf1e1d163001bd16380
eddie <eddie@ondelab.com>
2022-09-30 08:16:54 +0900

    유효성, 스키마 변경

--- src/main/java/com/cos/photogramstart/web/dto/auth/SignupDto.java
+++ src/main/java/com/cos/photogramstart/web/dto/auth/SignupDto.java
@@ -1,5 +1,8 @@
 package com.cos.photogramstart.web.dto.auth;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+
 import com.cos.photogramstart.domain.user.User;
 
 import lombok.Data;
@@ -7,9 +10,13 @@ import lombok.Data;
 //DTO는 해당 req에 담긴 데이터들을 담기 위함
 @Data // getter setter
 public class SignupDto {
+	@Max(20)
 	private String username;
+	@NotBlank
 	private String password;
+	@NotBlank
 	private String email;
+	@NotBlank
 	private String name;
 	
 	
