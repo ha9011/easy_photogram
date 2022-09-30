c0fc5fac918fb4f42020c31d441d22d863da1689
eddie <eddie@ondelab.com>
2022-09-30 11:13:46 +0900

    script로 바로 에러 대응하기

--- src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
+++ src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.photogramstart.handler.ex.CustomValidationException;
+import com.cos.photogramstart.util.Script;
 import com.cos.photogramstart.web.dto.CMRespDto;
 
 @RestController
@@ -24,9 +25,17 @@ public class ControllerExceptionHandler {
 	
 	@ExceptionHandler(CustomValidationException.class) //모든 에러중에 CustomValidationException을 낚아 챈다.
 	// 제네릭 ?는 추론이 가
-	public CMRespDto<?> validationException2(CustomValidationException e) {
-		log.info("--CustomValidationException 2낚아채기--");
-		return new CMRespDto<>(-1, e.getMessage(), e.getErrorMap()); // 또 문제되는게 runtimeException getMessage는 String임.. 우린 map으로 담고싶다 
-	}
+	
+	//public CMRespDto<?> validationException2(CustomValidationException e) {
+	public String validationException2(CustomValidationException e) {
 
+		log.info("--CustomValidationException 2낚아채기--");
+		//return new CMRespDto<>(-1, e.getMessage(), e.getErrorMap()); // 또 문제되는게 runtimeException getMessage는 String임.. 우린 map으로 담고싶다 
+		return Script.back(e.getErrorMap().toString());
+	}
+	
+	// CMRespDto vs script 비교
+	// 1. 클라이언트에 다이렉트 응답할 때는 script가 좋음
+	// 2.ajax통신 - CMRespDto
+	// 3.android - CMRespDto
 }
