86c5fb109a4b591580bd65c4236cdb48988916cd
eddie <eddie@ondelab.com>
2022-09-30 13:29:13 +0900

    test log.info 추가

--- src/main/java/com/cos/photogramstart/config/auth/PrincipalDetailsService.java
+++ src/main/java/com/cos/photogramstart/config/auth/PrincipalDetailsService.java
@@ -1,5 +1,7 @@
 package com.cos.photogramstart.config.auth;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -11,9 +13,13 @@ import org.springframework.stereotype.Service;
 //3. UserDetailsServic가 사라지고 대체해서 IOC에 격납이 
 public class PrincipalDetailsService implements UserDetailsService {
 	//시큐리티에서 .loginProcessingUrl("/auth/signin") -> POST  요청이 올경우 낚아챔!  
+	
+	
+	private static final Logger log = LoggerFactory.getLogger(PrincipalDetailsService.class);
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		log.info("--시큐리티 post 실행되??--");
 		// TODO Auto-generated method stub
 		return null;
 	}
