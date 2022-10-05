d6d8e53d1ed828b7dfd2b32b44caf31ed95e72b6
eddie <eddie@ondelab.com>
2022-10-05 20:28:52 +0900

    시큐리티에 세션정보 찾기, @어노테이션이용

--- src/main/java/com/cos/photogramstart/web/UserController.java
+++ src/main/java/com/cos/photogramstart/web/UserController.java
@@ -1,8 +1,16 @@
 package com.cos.photogramstart.web;
 
+import org.eclipse.jdt.internal.compiler.batch.Main.Logger;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import com.cos.photogramstart.config.auth.PrincipalDetails;
+
+import lombok.extern.java.Log;
 
 @Controller
 public class UserController {
@@ -13,7 +21,16 @@ public class UserController {
 	}	
 	
 	@GetMapping("/user/{id}/update")
-	public String update(@PathVariable String id)  {
+	public String update(@PathVariable String id, @AuthenticationPrincipal PrincipalDetails principalDetails)  {
+		//@AuthenticationPrincipal 을 통해서 시큐리티에 저장된 세션 쉽게 접근!!
+		System.out.println("세션정보 : " + principalDetails.getUser());
+		
+		// 어려운 방법  , 직접찾음 세션 정
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		PrincipalDetails mPrincipalDetails = (PrincipalDetails) auth.getPrincipal();
+		System.out.println("직접 세션정보 : " + mPrincipalDetails.getUser());
+
+		
 		return "user/update";
 	}	 
 }
